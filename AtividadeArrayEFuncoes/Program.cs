edba0095f5d9b47810a862963f04b6d1af47b8ad
felip <felip@DESKTOP-0R5TRUD>
2023-03-29 11:10:58 -0300

    Add project files.

---
+++ AtividadeArrayEFuncoes/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeArrayEFuncoes
+{
+    internal class Program
+    {
+        static int[] LerValores()
+        {
+            int[] valores = new int[10];
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.Write("Digite o {0}º valor: ", i + 1);
+                valores[i] = int.Parse(Console.ReadLine());
+            }
+            return valores;
+        }
+
+        static int MaiorValor(int[] valores)
+        {
+            return valores.Max();
+        }
+
+        static int MenorValor(int[] valores)
+        {
+            return valores.Min();
+        }
+
+        static double ValorMedio(int[] valores)
+        {
+            return valores.Average();
+        }
+
+        static int[] MaioresValores(int[] valores, int n)
+        {
+            return valores.OrderByDescending(x => x).Take(n).ToArray();
+        }
+
+        static int[] ValoresNegativos(int[] valores)
+        {
+            return valores.Where(x => x < 0).ToArray();
+        }
+
+        static int[] RemoverValor(int[] valores)
+        {
+            Console.Write("Digite o índice do valor a ser removido: ");
+            int indiceRemover = int.Parse(Console.ReadLine());
+            return valores.Where((x, i) => i != indiceRemover).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] valores = LerValores();
+            Console.WriteLine("Maior valor: {0}", MaiorValor(valores));
+            Console.WriteLine("Menor valor: {0}", MenorValor(valores));
+            Console.WriteLine("Valor médio: {0}", ValorMedio(valores));
+            Console.Write("Maiores valores: ");
+            foreach (int valor in MaioresValores(valores, 3))
+            {
+                Console.Write("{0} ", valor);
+            }
+            Console.WriteLine();
+            Console.Write("Valores negativos: ");
+            foreach (int valor in ValoresNegativos(valores))
+            {
+                Console.Write("{0} ", valor);
+            }
+            Console.WriteLine();
+            Console.Write("Valores da sequência: ");
+            foreach (int valor in valores)
+            {
+                Console.Write("{0} ", valor);
+            }
+            Console.WriteLine();
+            valores = RemoverValor(valores);
+            Console.Write("Valores da sequência após remoção: ");
+            foreach (int valor in valores)
+            {
+                Console.Write("{0} ", valor);
+            }
+            Console.WriteLine();
+        }
+
+       
+
+    }
+}
